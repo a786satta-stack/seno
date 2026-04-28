2e953f800c49fdb5e53af6a29bd8d43fa6565bfb
Renu Singh <gamerztarget167@google.com>
2026-04-28 13:11:59 +0530

    title change

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -8,7 +8,7 @@ const blogPosts = [
   {
     id: 1,
     slug: "blog1",
-    title: "Understanding  Charts: A Comprehensive Guide",
+    title: "Learn  Charts: A Comprehensive Guide",
     excerpt: "Learn how 2 effectively read and analyze old and new record charts displayed in a clear and organized way to track historical patterns.",
     date: "April 19, 2026",
     author: "Admin",
