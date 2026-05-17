986c9469f29a55129f81480377f10e9d4b3f0c61
a786satta-stack <a786satta@gmail.com>
2026-05-17 15:46:26 +0530

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import Header from '@/components/frontend/Header';
 import Footer from '@/components/frontend/Footer';
 
-// Mock data  for blog posts to demonstrate the layout
+// Mock data for blog posts to demonstrate the layout
 const blogPosts = [ 
   {
     id: 1,
