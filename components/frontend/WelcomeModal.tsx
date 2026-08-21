862f62a1ceedc2919c18a94d1d993e7a5e5f72db
a786satta-stack <a786satta@gmail.com>
2026-08-21 12:40:05 +0530

    Update WelcomeModal.tsx

--- components/frontend/WelcomeModal.tsx
+++ components/frontend/WelcomeModal.tsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from 'react'
 import { X } from 'lucide-react'
 
-// 1Extend the Window interface to include gtag for analytics
+// 2Extend the Window interface to include gtag for analytics
 declare global {
   interface Window {
     gtag?: (
